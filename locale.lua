c43f3c5b1523c7fa5c241a937bef855ad10e4805
Debuger Home <debuger@debuger.eu>
2024-08-02 11:13:32 +0200

    0.6.2 r294

--- locale.lua
+++ locale.lua
@@ -47,6 +47,3 @@ function lang_exist(lang)
     for k, _ in pairs(_Lang) do if k == lang then return true end end
     return false
 end
-
-
--- function _L(key) return _Lang[DBGROPT.locale][key] or "**str_not_found**" end
